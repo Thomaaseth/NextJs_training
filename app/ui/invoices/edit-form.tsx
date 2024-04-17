d380584aadaabbc191aaaf479cc40aa9bf6ec77e
Thomas D <thomas.demathieu@gmail.com>
2024-04-17 17:01:56 +0200

    invoice done

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -9,7 +9,7 @@ import {
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
-import { updateInfo } from '@/app/lib/actions';
+import { updateInvoice } from '@/app/lib/actions';
 
 export default function EditInvoiceForm({
   invoice,
@@ -18,11 +18,11 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
-  const updateInvoiceWithId = updateInfo.bind(null, invoice.id);
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   return (
    
     <form action={updateInvoiceWithId}>
-            <input type="hidden" name="id" value={invoice.id} />
+      <input type="hidden" name="id" value={invoice.id} />
 
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
